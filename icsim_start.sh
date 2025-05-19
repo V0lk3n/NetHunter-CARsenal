b7d5a4c5822d79224f542633aff03dfded8eba6c
V0lk3n <22322762+V0lk3n@users.noreply.github.com>
2025-05-20 00:12:41 +0200

    Fix icsim_start.sh structure

--- icsim_start.sh
+++ icsim_start.sh
@@ -6,15 +6,15 @@ CAN_IFACE="$2"
 
 export DISPLAY=:1
 
-echo "Starting ICSim... Please wait.\n"
+echo -e "Starting ICSim... Please wait.\n"
 
 # launch virtual display
-Xvfb :1 -screen 0 692x350x16 & > /dev/null 2>&1
+Xvfb :1 -screen 0 692x350x16 > /dev/null 2>&1 &
 echo "✅ Virtual Display Started!"
 
 # launch window manager
 sleep 5
-fluxbox & > /dev/null 2>&1
+fluxbox > /dev/null 2>&1 &
 echo "✅ Window manager Started!"
 
 # launch VNC Server
@@ -23,10 +23,10 @@ x11vnc -display :1 -nopw -forever -bg -rfbport 5900 > /dev/null 2>&1
 echo "✅ VNC Server Started!"
 
 cd /opt/car_hacking/ICSim/builddir
-./icsim "$RANDOMIZE" "$CAN_IFACE" & > /dev/null 2>&1
+./icsim "$RANDOMIZE" "$CAN_IFACE" > /dev/null 2>&1 &
 echo "✅ ICSim running..."
 
 # run noVNC proxy
 cd /opt/noVNC
-./utils/novnc_proxy --vnc localhost:5900 --listen 6080 > /dev/null 2>&1
+./utils/novnc_proxy --vnc localhost:5900 --listen 6080 > /dev/null 2>&1 &
 echo "✅ noVNC is running! Go back to ICSim and refresh display!"
