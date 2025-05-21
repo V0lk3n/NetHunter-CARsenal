a3bf2c404298e6dfaf2b0ca0078fb210b9cd3d4c
V0lk3n <22322762+V0lk3n@users.noreply.github.com>
2025-05-21 22:54:15 +0200

    Update icsim_start.sh

--- icsim_start.sh
+++ icsim_start.sh
@@ -8,19 +8,20 @@ echo "Starting ICSim... Please wait.\n"
 
 # ICSim
 # launch virtual display
+export DISPLAY=:1
 Xvfb :1 -screen 0 692x350x16 > /dev/null 2>&1 &
 echo "✅ ICSim Virtual Display Started!"
 # Launch window manager
 sleep 5
-DISPLAY=:1 fluxbox > /dev/null 2>&1 &
+fluxbox > /dev/null 2>&1 &
 echo "✅ ICSim Window manager Started!"
 # launch VNC Server
 sleep 2
-DISPLAY=:1 x11vnc -display :1 -nopw -forever -bg -rfbport 5900 > /dev/null 2>&1
+x11vnc -display :1 -nopw -forever -bg -rfbport 5900 > /dev/null 2>&1
 echo "✅ ICSim VNC Server Started!"
 # launch ICSim
 cd /opt/car_hacking/ICSim/builddir
-DISPLAY=:1 ./icsim -s "$SEED" "$CAN_IFACE" > /dev/null 2>&1 &    
+./icsim -s "$SEED" "$CAN_IFACE" > /dev/null 2>&1 &    
 echo "✅ ICSim Started on $CAN_IFACE with seed: $SEED"
 # run noVNC proxy
 cd /opt/noVNC
@@ -31,19 +32,20 @@ echo "\nStarting Controls... Please wait.\n"
 
 # Controls
 # launch virtual display
+export DISPLAY=:2
 Xvfb :2 -screen 0 835x608x16 > /dev/null 2>&1 &
 echo "✅ Controls Virtual Display Started!"
 # launch window manager
 sleep 5
-DISPLAY=:2 fluxbox > /dev/null 2>&1 &
+fluxbox > /dev/null 2>&1 &
 echo "✅ Controls Window manager Started!"
 # launch VNC Server
 sleep 2
-DISPLAY=:2 x11vnc -display :2 -nopw -forever -bg -rfbport 5901 > /dev/null 2>&1
+x11vnc -display :2 -nopw -forever -bg -rfbport 5901 > /dev/null 2>&1
 echo "✅ Controls VNC Server Started!"
 # Run controls with the correct seed
 cd /opt/car_hacking/ICSim/builddir
-DISPLAY=:2 ./controls -s "$SEED" "$CAN_IFACE" "$LEVEL" > /dev/null 2>&1 &
+./controls "$CAN_IFACE" -s "$SEED" "$LEVEL" > /dev/null 2>&1 &
 echo "✅ Controls Started!"
 # run noVNC proxy
 cd /opt/noVNC
