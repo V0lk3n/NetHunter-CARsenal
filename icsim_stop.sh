b6c869ce6a1b264c81d524d84bfcae2dc877be7b
V0lk3n <22322762+V0lk3n@users.noreply.github.com>
2025-05-23 01:12:30 +0200

    Stop controls added

--- icsim_stop.sh
+++ icsim_stop.sh
@@ -6,6 +6,10 @@ echo "Stopping ICSim...\n"
 pkill -f "/opt/car_hacking/ICSim/builddir/icsim"
 echo "✅ ICSim Stopped!"
 
+# Kill ICSim
+pkill -f "/opt/car_hacking/ICSim/builddir/controls"
+echo "✅ Controls Stopped!"
+
 # Kill noVNC proxy
 pkill -f "novnc_proxy"
 echo "✅ noVNC Stopped!"
@@ -18,8 +22,10 @@ echo "✅ VNC Server Stopped!"
 pkill -f "fluxbox"
 echo "✅ Window manager Stopped!"
 
-# Kill Xvfb
-pkill -f "Xvfb :1"
+# Kill Xvfb ICSim and Controls display
+pkill -f "ICSim Xvfb :1"
 echo "✅ Virtual Display Stopped!"
+pkill -f "Xvfb :2"
+echo "✅ Controls Virtual Display Stopped!"
 
 echo "\n✅ All ICSim-related processes Stopped."
