a5c5917b95a05f9cc5d9f5a780d25e3e1f9e9311
V0lk3n <22322762+V0lk3n@users.noreply.github.com>
2025-09-04 01:20:33 +0200

    Update carsenal_setup.sh

--- carsenal_setup.sh
+++ carsenal_setup.sh
@@ -38,7 +38,7 @@ setup() {
 
     sudo apt update
 
-    sudo apt install -y autoconf cmake expect fluxbox git libconfig-dev libsdl2-dev libsdl2-image-dev libsocketcan-dev make maven meson python3-pip python3-venv websockify x11vnc xserver-xephyr xvfb python3-can metasploit-framework
+    sudo apt install -y autoconf cmake expect fluxbox git libconfig-dev libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsocketcan-dev make maven meson python3-pip python3-venv websockify x11vnc xserver-xephyr xvfb python3-can metasploit-framework
 
     sudo gem install serialport
 
@@ -73,7 +73,10 @@ setup() {
         "cd /opt && sudo git clone https://github.com/novnc/noVNC.git"
 
     checkAndInstall "ICSIM" "/opt/car_hacking/ICSim/builddir/icsim" "/opt/car_hacking/ICSim" \
-        "cd /opt/car_hacking && sudo git clone https://github.com/V0lk3n/ICSim.git && cd ICSim && sudo cp /opt/car_hacking/can-utils/lib.o . && sudo meson setup builddir && cd builddir && sudo meson compile && cd /opt/car_hacking && wget https://raw.githubusercontent.com/V0lk3n/NetHunter-CARsenal/refs/heads/main/icsim_start.sh && wget https://raw.githubusercontent.com/V0lk3n/NetHunter-CARsenal/refs/heads/main/icsim_stop.sh && wget wget https://raw.githubusercontent.com/V0lk3n/NetHunter-CARsenal/refs/heads/main/icsim_service.sh && sudo chmod +x icsim_start.sh icsim_stop.sh icsim_service.sh"
+        "cd /opt/car_hacking && sudo git clone https://github.com/V0lk3n/ICSim.git && cd ICSim && sudo cp /opt/car_hacking/can-utils/lib.o . && sudo meson setup builddir && cd builddir && sudo meson compile && cd /opt/car_hacking && && wget https://raw.githubusercontent.com/V0lk3n/NetHunter-CARsenal/refs/heads/main/icsim_service.sh && sudo chmod +x icsim_service.sh"
+
+    checkAndInstall "UDSim" "/opt/car_hacking/UDSim/src/udsim" "/opt/car_hacking/UDSim" \
+        "cd /opt/car_hacking && sudo git clone https://github.com/V0lk3n/UDSim.git && cd UDSim/src && ./make && cd /opt/car_hacking && wget https://raw.githubusercontent.com/V0lk3n/NetHunter-CARsenal/refs/heads/main/udsim_service.sh && sudo chmod +x udsim_service.sh"
 
     checkAndInstall "sequence_finder.sh" "/opt/car_hacking/sequence_finder.sh" "/opt/car_hacking" \
         "cd /opt/car_hacking && sudo wget https://raw.githubusercontent.com/V0lk3n/NetHunter-CARsenal/refs/heads/main/sequence_finder.sh && sudo chmod +x sequence_finder.sh"
@@ -131,10 +134,13 @@ update() {
     checkAndUpdate "ICSIM" "/opt/car_hacking/ICSim/builddir/icsim" "/opt/car_hacking/ICSim" \
         "cd /opt/car_hacking/ICSim && old=\$(git rev-parse HEAD) && sudo git pull && new=\$(git rev-parse HEAD) && [[ \"\$old\" != \"\$new\" ]] && { echo 'Update detected! Updating...'; sudo meson setup builddir && sudo cp /opt/car_hacking/can-utils/lib.o . && cd builddir && sudo meson compile; }; \
          cd /opt/car_hacking && sudo rm -f icsim_start.sh icsim_stop.sh && \
-         sudo wget https://raw.githubusercontent.com/V0lk3n/NetHunter-CARsenal/refs/heads/main/icsim_start.sh && \
-         sudo wget https://raw.githubusercontent.com/V0lk3n/NetHunter-CARsenal/refs/heads/main/icsim_stop.sh && \
          sudo wget https://raw.githubusercontent.com/V0lk3n/NetHunter-CARsenal/refs/heads/main/icsim_service.sh && \
-         sudo chmod +x icsim_start.sh icsim_stop.sh icsim_service.sh"
+         sudo chmod +x icsim_service.sh"
+
+    checkAndUpdate "UDSim" "/opt/car_hacking/UDSim/src/udsim" "/opt/car_hacking/UDSim" \
+        "cd /opt/car_hacking/UDSim && old=\$(git rev-parse HEAD) && sudo git pull && new=\$(git rev-parse HEAD) && [[ \"\$old\" != \"\$new\" ]] && { echo 'Update detected! Updating...'; cd /opt/car_hacking/UDSim/src && make; }; \
+         sudo wget https://raw.githubusercontent.com/V0lk3n/NetHunter-CARsenal/refs/heads/main/udsim_service.sh && \
+         sudo chmod +x udsim_service.sh"
 
     checkAndUpdate "can_reset.sh" "/opt/car_hacking/can_reset.sh" "/opt/car_hacking" \
         "cd /opt/car_hacking && sudo rm -f can_reset.sh && wget https://raw.githubusercontent.com/V0lk3n/NetHunter-CARsenal/refs/heads/main/can_reset.sh && sudo chmod +x can_reset.sh"
